f7d71524179c558bf296d4d3c4be9100787637a3
Robbie <robert.cronin@uqconnect.edu.au>
2017-12-17 20:07:01 +1000

    added the initial function

--- snake_game.js
+++ snake_game.js
@@ -1,1 +1,9 @@
 <canvas id = "gc" width = width="400" height="400"></canvas>
+<script>
+  window.onload=loaded() {
+    canv=document.getElementById("gc");
+    context = canv.getContext("2d");
+    document.addEventListener("keydown", keyPush);
+    setInterval(game, 1000/15);
+  }
+</script>
